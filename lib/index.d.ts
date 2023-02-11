1d297aa87be706fb0e341752196bc689903a968b
Nerus-Zaumu <neruszaumuy@gmail.com>
2023-02-11 20:26:43 +0100

    (fix): Updated build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,14 @@
-import { Country } from "./types";
+import { Continent, Country } from "./utils/types";
 export declare const verifyCountryExistence: (country: string) => boolean;
-export declare const getCountryCode: (countryObj: Partial<Pick<Country, 'name' | 'dial_code'>>) => Country | "Country you are looking for does not exist";
-export declare const getCountryDialCode: (countryObj: Partial<Pick<Country, 'code' | 'name'>>) => Country | "No country with the name and or code entered exists";
-export declare const getCountryName: (countryObj: Partial<Pick<Country, 'code' | 'dial_code'>>) => Country | "Country you are looking for does not exist";
-export declare const getCountryInfo: (countryObj: Partial<Country>) => void;
+export declare const getCountryCode: (countryObj: Partial<Pick<Country, 'name' | 'dial_code'>>) => string | Error;
+export declare const getCountryDialCode: (countryObj: Partial<Pick<Country, 'code' | 'name'>>) => string | Error;
+export declare const getCountryName: (countryObj: Partial<Pick<Country, 'code' | 'dial_code'>>) => string | Error;
+export declare const getCountryInfo: (countryObj: Partial<Omit<Country, 'continent'>>) => Country | Error;
+export declare const verifyIfCountryInContinent: (countryObj: Partial<Pick<Country, 'name' | 'dial_code' | 'code'>>, continent: Continent) => Country | Error;
+export declare const getContinentOfCountry: (countryObj: Partial<Pick<Country, 'name' | 'dial_code' | 'code'>>) => Continent | Error;
+export declare const getCountriesOfContinent: (continent: Continent, limit?: number) => Country[] | Error;
+export declare const getAllCountries: (limit?: number) => Country[];
+export declare const sortCountries: (sortObj?: {
+    data?: Country[];
+    limit?: number;
+}) => Country[] | Error;
---
+++ lib/utils/compare.util.d.ts
@@ -0,0 +1,2 @@
+import { Country } from './../../lib/types.d';
+export declare const compareFunc: (a: Country, b: Country) => 1 | -1 | 0;
